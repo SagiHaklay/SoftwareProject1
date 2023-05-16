cd8304685e43aa8f73384aa41a1e52b56c895c93
Ohad Elkayam <ohadelkayam@mail.tau.ac.il>
2023-05-16 19:19:54 +0300

    bug fixed and added validity check what happens if len(args)>4 is missing

--- kmeans.py
+++ kmeans.py
@@ -1,11 +1,11 @@
 import math
 import sys
+
 eps = 0.001
-K = int(sys.argv[1])
-iter = 200 if len(sys.argv) == 3 else int(sys.argv[2])
+
 
 class Point:
-    def __init__(self, data=[]) -> None:
+    def __init__(self, data) -> None:
         self.data = data
 
     def __str__(self) -> str:
@@ -56,7 +56,7 @@ class Cluster:
 
 def read_from_file():
     list_of_points = []
-    f = open(sys.argv.pop(), "r")
+    f = open(sys.argv[-1], "r")
     for line in f:
         current_line = line.split(",")
         current_line = [float(x) for x in current_line]
@@ -65,16 +65,59 @@ def read_from_file():
     return list_of_points
 
 
-# need to add sanity check and termination conditions
+def k_error():
+    print("Invalid number of clusters!")
+    sys.exit()
+
+
+def iter_error():
+    print("Invalid maximum iteration!")
+    sys.exit()
+
+
+def both_errors():
+    print("Invalid number of clusters!")
+    print("Invalid maximum iteration!")
+    sys.exit()
+
 
 datapoints = read_from_file()
+# validity check
+args_len = len(sys.argv)
+if args_len < 3:
+    k_error()
+# if args_len > 4:
+# need to ask what to return
+if args_len == 3:
+    if not sys.argv[1].isdigit() or int(sys.argv[1]) <= 1 or int(sys.argv[1]) >= len(datapoints):
+        k_error()
+else:  # args_len == 4
+    if (not sys.argv[1].isdigit()) and (not sys.argv[2].isdigit()):
+        both_errors()
+    # there might be only one that isn't a number
+    if not sys.argv[1].isdigit() and (int(sys.argv[2]) <= 1 or int(sys.argv[2]) >= 1000):
+        both_errors()
+    if not sys.argv[2].isdigit() and (int(sys.argv[1]) <= 1 or int(sys.argv[1]) >= len(datapoints)):
+        both_errors()
+
+K = int(sys.argv[1])
+iter = 200 if args_len == 3 else int(sys.argv[2])
+
+if (K <= 1 or K >= len(datapoints)) and (iter <= 1 or iter >= 1000):
+    both_errors()
+if K <= 1 or K >= len(datapoints):
+    k_error()
+if iter <= 1 or iter >= 1000:
+    iter_error()
+
+# beginning of algorithm
 clusters = []
 for i in range(K):
     clusters.append(Cluster(datapoints[i]))
 i = 0
 converged = []
 for i in range(iter):
-    for point in datapoints: #adding each point to the nearest cluster's points list
+    for point in datapoints:  # adding each point to the nearest cluster's points list
         nearest_cluster = point.match_cluster(clusters)
         nearest_cluster.add_point(point)
     for cluster in clusters:
@@ -84,4 +127,7 @@ for i in range(iter):
         break
     else:
         converged.clear()
-print([x.centroid.data for x in clusters])
+
+for cluster in clusters:
+    cluster.centroid.data = [float("%.4f" % i) for i in cluster.centroid.data]
+    print(*cluster.centroid.data, sep=",")
